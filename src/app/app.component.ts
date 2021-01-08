62effbbbeef24ac1142aa940f8883766e0227507
victorchei <60154090+victorchei@users.noreply.github.com>
2021-01-08 17:13:00 +0200

    set params to child component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 
+export interface Post {
+  title: string
+  text: string
+  id?: number
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-
-  now: Date = new Date()
-
+  posts: Post[] = [
+    {title: 'Хочу выучить Angular компоненты', text: 'Я все еще учу компоненты', id: 1},
+    {title: 'Следующий блок', text: 'Будет про директивы и еще про пайпы', id: 2}
+  ]
 }
