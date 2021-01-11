8801e17a06ca49637817036d42512c7693cca1f3
victorchei <60154090+victorchei@users.noreply.github.com>
2021-01-11 18:18:50 +0200

    my project toUppercase one letter

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,16 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'Angular';
+  projectName = 'MY PROJECT'
+  x = 0;
+  constructor() {
+
+    setInterval( () => {
+      let str: any = this.projectName.toLowerCase().split('');
+      str[this.x] = str[this.x].toUpperCase();
+      this.projectName = str.join('');
+      this.x++;
+      if(this.projectName.length === this.x) this.x = 0
+    }, 1000)
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { MainComponent } from './pages/main/main.component';
+import { ListComponent } from './pades/list/list.component';
+import { CalcComponent } from './pades/calc2/calc.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainComponent,
+    ListComponent,
+    CalcComponent
   ],
   imports: [
     BrowserModule
